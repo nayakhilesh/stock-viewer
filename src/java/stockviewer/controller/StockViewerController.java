616b0c85a4ec47dadb85a668da9ba7e0695e91f4
Akhilesh Nayak <nayakhilesh@gmail.com>
2013-08-21 21:57:09 -0400

    added DB

--- src/java/stockviewer/controller/StockViewerController.java
+++ src/java/stockviewer/controller/StockViewerController.java
@@ -1,5 +1,6 @@
 package stockviewer.controller;
 
+import java.sql.SQLException;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
@@ -7,6 +8,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import stockviewer.stock.StockDao;
 import stockviewer.stock.StockData;
 import stockviewer.stock.StockDataException;
 import stockviewer.stock.StockDataSource;
@@ -19,10 +21,12 @@ public class StockViewerController implements Controller {
 			.getLogger(StockViewerController.class);
 
 	private StockDataSource ds;
+	private StockDao dao;
 	private List<View> views;
 
-	public StockViewerController(StockDataSource ds) {
+	public StockViewerController(StockDataSource ds, StockDao dao) {
 		this.ds = ds;
+		this.dao = dao;
 		views = new LinkedList<View>();
 	}
 
@@ -42,8 +46,40 @@ public class StockViewerController implements Controller {
 
 		LOG.info("Retrieving stock data");
 
-		List<StockData> data1 = ds.getStockData(tickerSymbol1, from, to);
-		List<StockData> data2 = ds.getStockData(tickerSymbol2, from, to);
+		List<StockData> data1 = null;
+		List<StockData> data2 = null;
+
+		try {
+
+			data1 = ds.getStockData(tickerSymbol1, from, to);
+			data2 = ds.getStockData(tickerSymbol2, from, to);
+
+			if (dao != null) {
+				dao.insertStockInfo(new StockInfo(tickerSymbol1, data1));
+				dao.insertStockInfo(new StockInfo(tickerSymbol2, data2));
+			}
+
+		} catch (StockDataException sde) {
+
+			if (dao == null)
+				throw sde;
+			
+			LOG.error("Error getting stock data from:" + ds);
+			try {
+				LOG.info("Trying to get from DB");
+				data1 = dao.getStockData(tickerSymbol1, from, to);
+				data2 = dao.getStockData(tickerSymbol2, from, to);
+			} catch (Exception e) {
+				LOG.error("Error retrieving stock info from DB", e);
+			}
+
+			if (data1 == null || data2 == null || data1.isEmpty()
+					|| data2.isEmpty())
+				throw sde;
+
+		} catch (SQLException e) {
+			LOG.error("Error storing stock info in DB", e);
+		}
 
 		LOG.info("Updating registered Views");
 
